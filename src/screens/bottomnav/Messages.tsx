34f941ceefe12fcefb1f0ee104b93d016a803b48
chandan-fr <georgesteve31019@gmail.com>
2023-12-21 13:30:04 +0530

    message page done

--- src/screens/bottomnav/Messages.tsx
+++ src/screens/bottomnav/Messages.tsx
@@ -43,8 +43,68 @@ const Messages: React.FC<MessagesProps> = ({ navigation }) => {
         </View>
 
         {/* diet programs  */}
-        <View>
-          
+        <View style={styles.dpWrap}>
+          <Text style={styles.secHead}>Diet Programs</Text>
+
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            <View style={styles.dpCont}>
+              <View style={styles.dpContBox}></View>
+
+              <View style={styles.dpContBody}>
+                <Text style={styles.contBodyHead}>Food Diet Plan</Text>
+                <Text style={styles.contBodySubHead}>February, 12 2023</Text>
+              </View>
+            </View>
+
+            <View style={[styles.dpCont, { marginLeft: 10 }]}>
+              <View style={styles.dpContBox}></View>
+
+              <View style={styles.dpContBody}>
+                <Text style={styles.contBodyHead}>Exercise Plan</Text>
+                <Text style={styles.contBodySubHead}>January, 16 2023</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* diet recipes */}
+        <View style={styles.drWrap}>
+          <Text style={styles.secHead}>Diet Recipes</Text>
+
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={[styles.drCont, {marginTop: 10}]}>
+              <View style={styles.drContBox}></View>
+
+              <View style={styles.drContBody}>
+                <View>
+                  <Text style={styles.contBodyHead}>Oatmeal Salad</Text>
+                  <Text style={styles.contBodySubHead}>March, 10 2023</Text>
+                </View>
+
+                <TouchableOpacity style={styles.btn}>
+                  <Text style={styles.btnText}>ADD TO CART</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <View style={[styles.drCont, { marginVertical: 10, }]}>
+              <View style={styles.drContBox}></View>
+
+              <View style={styles.drContBody}>
+                <View>
+                  <Text style={styles.contBodyHead}>Banana Smoothie</Text>
+                  <Text style={styles.contBodySubHead}>March, 12 2023</Text>
+                </View>
+
+                <TouchableOpacity style={styles.btn}>
+                  <Text style={styles.btnText}>ADD TO CART</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </View>
     </SafeAreaView>
@@ -104,5 +164,74 @@ const styles = StyleSheet.create({
     height: 20,
     tintColor: darkShade2,
     marginLeft: 15,
+  },
+  dpWrap: {
+    marginTop: 25,
+  },
+  secHead: {
+    color: dark,
+    fontSize: 17,
+    fontWeight: "600",
+    marginBottom: 18,
+  },
+  dpCont: {
+    borderRadius: 12,
+    backgroundColor: light,
+    padding: 12,
+    flexDirection: "row",
+    width: width / 1.8,
+    alignItems: "center",
+  },
+  dpContBox: {
+    width: 70,
+    height: 70,
+    backgroundColor: greenShade3,
+    borderRadius: 12,
+  },
+  dpContBody: {
+    flex: 1,
+    marginLeft: 15,
+  },
+  contBodyHead: {
+    color: dark,
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  contBodySubHead: {
+    color: darkShade1,
+    fontSize: 13,
+    marginTop: 7,
+  },
+  drWrap: {
+    marginTop: 25,
+    flex: 1,
+  },
+  drCont: {
+    backgroundColor: light,
+    borderRadius: 12,
+    padding: 12,
+  },
+  drContBox: {
+    backgroundColor: greenShade3,
+    borderRadius: 12,
+    flex: 1,
+    height: 160,
+  },
+  drContBody: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-between',
+    marginVertical: 15,
+  },
+  btn: {
+    backgroundColor: green,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+  },
+  btnText: {
+    color: light,
+    fontSize: 13,
+    fontWeight: 'bold',
   },
 });
